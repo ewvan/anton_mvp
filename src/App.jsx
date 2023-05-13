8f4b4f87a96dbb64c3e7190365a42656f1fe82db
Leontii <72815747+loikx@users.noreply.github.com>
2023-05-13 22:44:29 +0300

    Update App.jsx
    First commit with 4 if's

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,106 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const checkWheelAndBody = (wheel, body) => {
+  return wheel.radius() < body.wheel_radius();
+}
+
+const checkSummaryWeight = (engine, roof, body) => {
+  return body.weight_capacity() >= engine.weight() + roof.weight();
+}
+
+const checkRoofAndBody = (roof, body) => {
+  return body.length() > roof.length() && body.width() == roof.width();
+}
+
+const checkEngineAndBody = (engine, body) => {
+  return engine.height() < body.height() && engine.width() < body.width();
+}
+
+class Car {
+  constructor(engine, body, roof, wheel) {
+    this.engine = engine;
+    this.body = body;
+    this.roof = roof;
+    this.wheel = wheel;
+  }
+
+  engine() { return this.engine; }
+
+  body() { return this.body; }
+
+  roof() { return this.roof; }
+
+  wheel() { return this.wheel; }
+}
+
+
+class Engine {
+  constructor(height, width, length, weight) {
+    this.height = height;
+    this.width = width;
+    this.length = length;
+    this.weight = weight;
+  }
+
+  height() { return this.height; }
+
+  width() { return this.width; }
+
+  length() { return this.length; }
+
+  weight() { return this.weight; }
+}
+
+
+class Body {
+  constructor(height, width, length, weightCapacity, wheels_radius) {
+    this.height = height;
+    this.width = width;
+    this.length = length;
+    this.weight_capacity = weightCapacity;
+    this.wheel_radius = wheels_radius;
+  }
+
+  height() { return this.height; }
+
+  width() { return this.width; }
+
+  length() { return this.length; }
+
+  weight_capacity() { return this.weight_capacity; }
+
+  wheel_radius() { return this.wheel_radius; }
+}
+
+class Roof {
+  constructor(height, width, length) {
+    this.height = height;
+    this.width = width;
+    this.length = length;
+  }
+
+  height() { return this.height; }
+
+  width() { return this.width; }
+
+  length() { return this.length; }
+}
+
+
+class Wheel {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  radius() { return this.radius; }
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div></div>
   )
 }
 
